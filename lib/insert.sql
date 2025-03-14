4a378291fdd5e3dad93c280c31fd1b0b31db95d6
Steve <smmaina2001@gmail.com>
2025-03-14 15:23:36 +0300

    14 passed tests

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 445, "M", "Angry", "Bright-Red", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 45, "M","Angry", "Orange", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 445, "F", "Angry", "Green", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("SErgeant Brown", 33, "M", "Angry", "Army-green", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 21, "F", "Angry", "pink", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 133, "F", "not-Angry", "Ginger", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 133, "F", "Angry", "not RED", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 133, "F", "Happy", "not RED", 1);
+
+
+  -- name TEXT, 
+  -- age INTEGER, 
+  -- sex TEXT, 
+  -- color TEXT, 
+  -- temperament TEXT, 
+  -- alive INTEGER
